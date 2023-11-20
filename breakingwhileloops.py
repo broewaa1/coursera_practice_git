ada8325e078d0fee3414b8e6a53a624b624c5538
broewaa1 <oropeze1@uci.edu>
2023-11-20 14:43:45 -0800

    found a bug that causes an infinite loop

---
+++ breakingwhileloops.py
@@ -0,0 +1,10 @@
+#causes a infinite loop
+
+def print_range(start, end):
+    # Loop through the numbers from start to end
+    n = start
+    while n <= end:
+        print(n)
+        
+        
+print(1,5) #should print 1 2 3 4 5
