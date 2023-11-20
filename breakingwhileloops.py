d8f9358093878d756d42ba2c5a0abbf0745373aa
broewaa1 <oropeze1@uci.edu>
2023-11-20 14:54:46 -0800

    had to add "n+=1" and some comments

--- breakingwhileloops.py
+++ breakingwhileloops.py
@@ -1,10 +1,14 @@
 #causes a infinite loop
 
+
+#should print 1 2 3 4 5 
 def print_range(start, end):
-    # Loop through the numbers from start to end
+    # loop through the numbers from start to end
+    #print the numbers from "1 2 3 4 5"
     n = start
-    while n <= end:
+    while n<=end:
         print(n)
-        
-        
-print(1,5) #should print 1 2 3 4 5
+        n += 1 # if you don't add this line, it will be an infinite loop
+        #because the value of n will never change
+        #print the numbers from "1 2 3 4 5"
+print_range(1, 5)  # Should print 1 2 3 4 5 (each number on its own line)
