b9e8f956d1f1747213b5d95594f234e99e381dd2
broewaa1 <oropeze1@uci.edu>
2023-11-20 14:37:55 -0800

    added notes

--- whileloops.py
+++ whileloops.py
@@ -6,3 +6,10 @@ def count_down(start_number):
   print("Zero!")
 
 count_down(3)
+
+'''
+NameError is a built-in Python function that informs you something is not defined.
+so you need to assign a variable to a value before you can use it.
+
+
+'''
